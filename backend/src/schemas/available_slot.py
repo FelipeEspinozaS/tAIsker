fac1b6e111ea72b80d739572381819027466fcd9
Felipe Espinoza <felipe.espinozas@uc.cl>
2025-08-01 19:24:06 -0400

    Añadir validación de horarios en available_slots

--- backend/src/schemas/available_slot.py
+++ backend/src/schemas/available_slot.py
@@ -1,10 +1,19 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, root_validator
 from database import time, date
 
 class AvailableSlotCreateSchema(BaseModel):
   date: date
   start_time: time
   end_time: time
+
+  @root_validator
+  def validate_times_renge(cls, values):
+    start = values.get("start_time")
+    end = values.get("end_time")
+    if start and end and start >= end:
+      raise ValueError("Start time must be before end time")
+    
+    return values
 
 class AvailableSlotSchema(AvailableSlotCreateSchema):
   id: int
